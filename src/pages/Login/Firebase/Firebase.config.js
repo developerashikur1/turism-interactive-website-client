92054334c5560c7ca788b6e843fd32718ee41272
developerashikur1 <masik4506@gmail.com>
2021-10-31 21:12:28 +0600

    ignore

--- src/pages/Login/Firebase/Firebase.config.js
+++ src/pages/Login/Firebase/Firebase.config.js
@@ -1,18 +1,22 @@
 const firebaseConfig = {
-    // apiKey: "AIzaSyCQDnFSyTyr8vxTycuNdAVflWXc8sgU1wc",
-    // authDomain: "vm-tours.firebaseapp.com",
-    // projectId: "vm-tours",
-    // storageBucket: "vm-tours.appspot.com",
-    // messagingSenderId: "648847580232",
-    // appId: "1:648847580232:web:a73728fb781aa1cc9c0c7d"
+    apiKey: "AIzaSyCQDnFSyTyr8vxTycuNdAVflWXc8sgU1wc",
+    authDomain: "vm-tours.firebaseapp.com",
+    projectId: "vm-tours",
+    storageBucket: "vm-tours.appspot.com",
+    messagingSenderId: "648847580232",
+    appId: "1:648847580232:web:a73728fb781aa1cc9c0c7d"
 
 
-    apiKey: process.env.REACT_APP_API_KEY,
-    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-    projectId: process.env.REACT_APP_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-    appId: process.env.REACT_APP_APP_ID,
+    // sorry sir git ignore is not working so that i can't do it .. thanks
+
+
+
+    // apiKey: process.env.REACT_APP_API_KEY,
+    // authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    // projectId: process.env.REACT_APP_PROJECT_ID,
+    // storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    // messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    // appId: process.env.REACT_APP_APP_ID,
 
 };
 
